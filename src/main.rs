7f1061e3258172083c1a99477da6bb1b915b0ceb
Luke Tomlin <luke.tomlin@gmail.com>
2019-08-18 10:11:45 +0100

    Basic handler-esque implementation using async_trait

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![feature(async_await)]
 
+use async_trait::async_trait;
 use std::env::args;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
+use tokio::net::tcp::split::TcpStreamWriteHalf;
 use std::error;
 use std::io::{Error, ErrorKind};
 use std::time::{Instant, Duration};
@@ -11,6 +13,39 @@ use tokio::timer::Delay;
 
 // use https://docs.rs/bytes/0.4.12/bytes/ for byte manipulation
 // use https://docs.rs/tokio/0.2.0-alpha.1/tokio/net/struct.TcpStream.html for streaming
+
+#[async_trait]
+trait Client {
+    async fn on_connect(&mut self);
+    async fn handle(&mut self, msg: &[u8]);
+}
+
+struct TrollHandler {
+    out: TcpStreamWriteHalf,
+}
+
+#[async_trait]
+impl Client for TrollHandler {
+    async fn on_connect(&mut self) {
+        let msg = "hello!".as_bytes();
+
+        println!("Sending on connect..");
+
+        if let Err(e) = self.out.write_all(&msg).await {
+            println!("Failed to write to socket; err = {:?}", e);
+        }
+    }
+
+    async fn handle(&mut self, msg: &[u8]) {
+        println!("Got back {:?}!", std::str::from_utf8(msg));
+
+        let msg = "hello!".as_bytes();
+
+        if let Err(e) = self.out.write_all(&msg).await {
+            println!("Failed to write to socket; err = {:?}", e);
+        }
+    }
+}
 
 const ADDR: &str = "127.0.0.1:8080";
 
@@ -36,8 +71,9 @@ async fn run_server() -> Result<(), Box<dyn error::Error>> {
                     }
                 };
 
-
                 println!("Got {:?}!", std::str::from_utf8(&buf[0..n]));
+
+                Delay::new(Instant::now().checked_add(Duration::from_millis(1000)).unwrap()).await;
 
                 // Write the data back
                 if let Err(e) = socket.write_all(&buf[0..n]).await {
@@ -49,20 +85,22 @@ async fn run_server() -> Result<(), Box<dyn error::Error>> {
     }
 }
 
-async fn run_client() -> Result<(), Box<dyn error::Error>> {
+async fn run_client<T, F>(factory: F) -> Result<(), Box<dyn error::Error>>
+    where T: Client,
+          F: Fn(TcpStreamWriteHalf) -> T {
     let addr = ADDR.parse()?;
 
-    let mut stream = TcpStream::connect(&addr).await?;
+    let stream = TcpStream::connect(&addr).await?;
+    let (mut read, write) = stream.split();
+    let mut client = (factory)(write);
 
     println!("Connected!");
+    client.on_connect().await;
 
     let mut buf = [0; 1024];
-    let msg: &str = "hello!";
-
-    stream.write_all(msg.as_bytes()).await?;
 
     loop {
-        let n = match stream.read(&mut buf).await {
+        let n = match read.read(&mut buf).await {
             Ok(n) if n == 0 => break, // socket was closed
             Ok(n) => n,
             Err(e) => {
@@ -71,14 +109,7 @@ async fn run_client() -> Result<(), Box<dyn error::Error>> {
             }
         };
 
-        println!("Got back {:?}!", std::str::from_utf8(&buf[0..n]));
-
-        Delay::new(Instant::now().checked_add(Duration::from_millis(1000)).unwrap()).await;
-
-        if let Err(e) = stream.write_all(&buf[0..n]).await {
-            println!("Failed to write to socket; err = {:?}", e);
-            break;
-        }
+        client.handle(&buf[0..n]).await;
     }
 
     Ok(())
@@ -92,7 +123,7 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
 
     if args.len() > 1 {
         match args[1].as_ref() {
-            "client" => run_client().await,
+            "client" => run_client(move |out| TrollHandler { out }).await,
             _ => run_server().await,
         }
     } else {
